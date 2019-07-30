a7a159280bf6d3e65d60b8599aa5af7a2e39851a
Kit <kit.jrs@gmail.com>
2019-07-30 11:52:50 +0700

    นำ google chart มาใส่

--- src/components/StatChart.js
+++ src/components/StatChart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Card } from "antd";
+import Chart from 'react-google-charts';
 
 export default class StatChart extends Component {
     render() {
@@ -11,7 +12,33 @@ export default class StatChart extends Component {
                         width: '100%'
                     }}    
                 >
-                    <p>Card Component</p>
+                    <Card title="Chart" style={{
+                    width: '100%'
+                }}>
+                    <Chart
+                        width={'100%'}
+                        height={'400px'}
+                        chartType="Line"
+                        loader={<div>Loading Chart</div>}
+                        data={[
+                            ['x', 'dogs'],
+                            [0, 0],
+                            [1, 10],
+                            [2, 23],
+                            [3, 17],
+                            [4, 18],
+                            [5, 9],
+                            [6, 11],
+                            [7, 27],
+                            [8, 33],
+                            [9, 40],
+                            [10, 32],
+                            [11, 35],
+                        ]}
+                        
+                        rootProps={{ 'data-testid': '1' }}
+                    />
+                </Card>
                 </Card>
             </div>
         )
