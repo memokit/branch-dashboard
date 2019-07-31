93039101668acded0e5f0d2114463800ffbd1964
Kit <kit.jrs@gmail.com>
2019-07-31 10:27:22 +0700

    ทำการ install redux-thunk และ redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import dashboardReducer from "./dashboard.reducer";
+import thunk from "redux-thunk";
+import { logger } from "redux-logger";
 
 export default function configureStore(){
-    let store = createStore(dashboardReducer);
+    let store = createStore(
+        dashboardReducer,
+        applyMiddleware(thunk,logger)
+    );
     return store;
 }
