1145c9e3f439aac13581fac28d353d4a365a62ff
Kit <kit.jrs@gmail.com>
2019-07-31 10:35:05 +0700

    เปลี่ยน app.js เป็น class component และเชื่อมเข้ากับ redux

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
+
 import HeaderBar from './components/HeaderBar';
-
-import { Provider } from "react-redux";
-import configureStore from "./redux/store";
-
-import { Layout, Menu, Row, Col } from 'antd';
 import MapBranch from './components/MapBranch';
 import StatChart from './components/StatChart';
+
+import { Layout, Menu, Row, Col } from 'antd';
 const { Header, Content, Footer } = Layout;
 
-const store = configureStore();
-
-function App() {
-  return (
-    <Provider store={store}>
+class App extends Component {
+  render() {
+    return (
+    
       <div>
         <Layout className="layout">
           <HeaderBar />
-          <Content style={{ padding: '0 50px' }}>
-            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+          <Content style={{
+            padding: '0 50px'
+          }}>
+            <div
+              style={{
+                background: '#fff',
+                padding: 24,
+                minHeight: 280
+              }}>
               <Row gutter={16}>
-                <Col span={12}>
-                  <MapBranch />
-                </Col>
-                <Col span={12}>
-                  <StatChart />
-                </Col>
+                <Col span={12}><MapBranch /></Col>
+                <Col span={12}><StatChart/></Col>
               </Row>
-
+  
             </div>
           </Content>
-          <Footer>React Redux Workshop ©2012-2019 Created by Nextflow.in.th</Footer>
-        </Layout>
+          <Footer style={{
+            textAlign: 'center'
+          }}>React Redux Workshop ©2012-2019 Created by Nextflow.in.th</Footer>
+        </Layout>,
       </div>
-    </Provider>
-  );
+      
+    );
+  }
 }
 
 export default App;
